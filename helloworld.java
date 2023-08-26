6d0c45316269793f4d93a695d3ae34d6b15c22ab
Laxman.Kamble <lgkamble68@gmail.com>
2023-08-26 20:31:55 +0530

    multiple

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
     public static void main (String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to git from clone!!!"); 
+        System.out.println("Welcome to git from clone - do multiple roles!!!"); 
     }
 }
